39fa52e0dcb74530d87024b9f41350c61fba14f0
Anonymous <None>
2018-04-04 13:48:02 +0300

    testrpc

--- truffle.js
+++ truffle.js
@@ -1,10 +1,19 @@
+const TestRPC = require("ethereumjs-testrpc");
 const HDWalletProvider = require("truffle-hdwallet-provider");
-const key = ["ce2eab5......."];
+const bufferKey = ["ce2eab5......."]; // buffer private key
 
 module.exports = {
     networks: {
+		test: {
+			network_id: "*",
+			provider: TestRPC.provider({
+				accounts: [10, 10, 10].map(balance => ({balance: balance + "0000000000000000000"})),
+				time: new Date(1522454400000)
+			}),
+			gas: 4000000
+		},
         ropsten: {
-            provider: () => new HDWalletProvider(key, "https://ropsten.infura.io"),
+            provider: () => new HDWalletProvider(bufferKey, "https://ropsten.infura.io"),
             network_id: 3,
             gas: 4500000,
             gasPrice: 40000000000
